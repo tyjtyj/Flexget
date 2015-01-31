3eda37f6487becf6ba9142fac2ad0965f5336a4f
Chase Sterling <chase.sterling@gmail.com>
2015-01-31 18:36:22 -0500

    Fix trakt_lookup tests

--- tests/test_trakt.py
+++ tests/test_trakt.py
@@ -22,11 +22,13 @@ class TestTraktLookup(FlexGetBase):
                 series_name: House
           test_episode_lookup:
             mock:
-              - {title: 'House.S01E02.HDTV.XViD-FlexGet'}
-              - {title: 'Doctor.Who.2005.S02E03.PDTV.XViD-FlexGet'}
+              - title: 'House.S01E02.HDTV.XViD-FlexGet'
+              - title: 'Doctor.Who.2005.S02E03.PDTV.XViD-FlexGet'
+              - title: 'Castle.2009.S03E01'
             series:
               - House
               - Doctor Who 2005
+              - Castle (2009)
           test_unknown_series:
             mock:
               - {title: 'Aoeu.Htns.S01E01.htvd'}
@@ -50,7 +52,7 @@ class TestTraktLookup(FlexGetBase):
         """trakt: Test Lookup (ONLINE)"""
         self.execute_task('test_show_lookup')
         entry = self.task.find_entry(title='House')
-        assert entry['trakt_series_id'] == 1399, \
+        assert entry['trakt_show_id'] == 1399, \
             'Trakt_ID should be 1339 is %s for %s' % (entry['trakt_series_id'], entry['series_name'])
         assert entry['trakt_series_status'] == 'ended', 'Series status should "ended". Got %s' \
                                                         % (entry['trakt_series_status'])
@@ -66,7 +68,9 @@ class TestTraktLookup(FlexGetBase):
             'runtime for %s is %s, should be "ended"' % (entry['title'], entry['trakt_series_status'])
         assert entry['afield'] == '73255Paternity', 'afield was not set correctly'
         assert self.task.find_entry(trakt_ep_name='School Reunion'), \
-            'Failed imdb lookup Doctor Who 2005 S02E03'
+            'Failed trakt lookup Doctor Who 2005 S02E03'
+        entry = self.task.find_entry(title='Castle.2009.S03E01')
+        assert entry['trakt_show_id'] == 1410, 'Year in series title should look up as year'
 
     @use_vcr
     def test_unknown_series(self):
@@ -74,19 +78,22 @@ class TestTraktLookup(FlexGetBase):
         self.execute_task('test_unknown_series')
         # Make sure it didn't make a false match
         entry = self.task.find_entry('accepted', title='Aoeu.Htns.S01E01.htvd')
-        assert entry.get('tvdb_id') is None, 'should not have populated tvdb data'
+        assert entry.get('trakt_show_id') is None, 'should not have populated trakt data'
 
     @use_vcr
     def test_date(self):
         self.execute_task('test_date')
         entry = self.task.find_entry(title='the daily show 2012-6-6')
-        assert entry.get('tvdb_id') is None, 'should not have populated trakt data'
+        assert entry.get('trakt_show_id') == 2211, 'should have populated trakt show data'
+        assert entry.get('trakt_episode_id') is None, 'should not have populated trakt episode data'
+
 
     @use_vcr
     def test_absolute(self):
         self.execute_task('test_absolute')
         entry = self.task.find_entry(title='naruto 128')
-        assert entry.get('tvdb_id') is None, 'should not have populated trakt data'
+        assert entry.get('trakt_show_id') == 46003, 'should have populated trakt show data'
+        assert entry.get('trakt_episode_id') is None, 'should not have populated trakt episode data'
 
 
 class TestTraktList(FlexGetBase):
